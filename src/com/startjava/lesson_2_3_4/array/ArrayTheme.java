0c0e28d507c3cb46604353c92b42966f9054b18a
movchan-d <den.a.movchan@gmail.com>
2023-05-19 14:03:28 +0300

    Lesson_4. HW. работа над ошибками

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -1,6 +1,5 @@
 package com.startjava.lesson_2_3_4.array;
 
-import org.jetbrains.annotations.NotNull;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -18,16 +17,18 @@ public class ArrayTheme {
 
     private static void reverseArray() {
         System.out.println("1. Реверс значений массива");
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+        int[] numbers = {5, 3, 2, 6, 7, 1, 4};
         len = numbers.length;
         // до модификации
         printIntArray(numbers);
 
         // перестановка значений в обратном порядке
-        for (int i = len; i > len / 2; i--) {
-            int tempNum = numbers[i - 1];
-            numbers[i - 1] = numbers[len - i];
-            numbers[len - i] = tempNum;
+        int middleCellElement = len / 2;
+        for (int i = 0; i < middleCellElement; i++) {
+            len--;
+            int tempNum = numbers[i];
+            numbers[i] = numbers[len];
+            numbers[len] = tempNum;
         }
 
         // отображение массива после модификации
@@ -37,44 +38,44 @@ public class ArrayTheme {
     private static void multiplyArrayElements() {
         System.out.println("\n2. Вывод произведения элементов массива");
 
-        int[] ints = new int[10];
-        len = ints.length;
+        int[] multipliers = new int[10];
+        len = multipliers.length;
         for (int i = 0; i < len; i++) {
-            ints[i] = i;
+            multipliers[i] = i;
         }
 
         int prod = 1;
         for (int i = 1; i < len - 1; i++) {
-            prod *= ints[i];
-            System.out.print(ints[i] + (i < len - 2 ? " * " : " = " + prod));
+            prod *= multipliers[i];
+            System.out.print(multipliers[i] + (i < len - 2 ? " * " : " = " + prod));
         }
-        System.out.println("\nЭлемент массива " + ints[0] + " имеет индекс " + 0);
-        System.out.println("Элемент массива " + ints[9] + " имеет индекс " + 9);
+        System.out.println("\nЭлемент массива " + multipliers[0] + " имеет индекс " + 0);
+        System.out.println("Элемент массива " + multipliers[len - 1] + " имеет индекс " + (len - 1));
     }
 
     private static void deleteArrayElements() {
         System.out.println("\n3. Удаление элементов массива");
 
         // Заполнение массива случайными вещественными числами
-        float[] floats = new float[15];
-        len = floats.length;
+        float[] rndFloats = new float[15];
+        len = rndFloats.length;
         for (int i = 0; i < len; i++) {
-            floats[i] = (float) Math.random();
+            rndFloats[i] = (float) Math.random();
         }
         System.out.println("Исходный массив:");
-        printFloatArray(floats);
+        printFloatArray(rndFloats);
 
         // Перезапись элементов массива
-        float middleElement = floats[len / 2];
+        float middleCellElement = rndFloats[len / 2];
         int countChangedElements = 0;
         for (int i = 0; i < len; i++) {
-            if (floats[i] > middleElement) {
-                floats[i] = 0;
+            if (rndFloats[i] > middleCellElement) {
+                rndFloats[i] = 0;
                 countChangedElements++;
             }
         }
         System.out.println("Измененный массив:");
-        printFloatArray(floats);
+        printFloatArray(rndFloats);
         System.out.println("Количество обнуленных элементов: " + countChangedElements);
     }
 
@@ -82,16 +83,16 @@ public class ArrayTheme {
         System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
 
         // Создание и заполнение массива
-        char[] chars = new char[26];
-        len = chars.length;
+        char[] alphabet = new char[26];
+        len = alphabet.length;
         for (int i = 0; i < len; i++) {
-            chars[i] = (char) ('A' + i);
+            alphabet[i] = (char) ('A' + i);
         }
 
         // Вывод элементов массива лесенкой в обратном порядке
         for (int i = len - 1; i >= 0; i--) {
             for (int j = i; j < len; j++) {
-                System.out.print(chars[j]);
+                System.out.print(alphabet[j]);
             }
             System.out.println();
         }
@@ -100,46 +101,54 @@ public class ArrayTheme {
     private static void generateUniqueNumbersArray() {
         System.out.println("\n5. Генерация уникальных чисел");
 
-        int[] ints = new int[30];
-        len = ints.length;
+        int[] uniqueInts = new int[30];
+        len = uniqueInts.length;
         Random rnd = new Random();
         for (int i = 0; i < len; i++) {
-            int num;
-            do {
+            int num = rnd.nextInt(40) + 60;
+            while (isUnique(uniqueInts, num)) {
                 num = rnd.nextInt(40) + 60;
-            } while (isUnique(ints, num));
-            ints[i] = num;
+            }
+            uniqueInts[i] = num;
         }
-        Arrays.sort(ints);
-        printIntArray(ints);
+        Arrays.sort(uniqueInts);
+        printIntArray(uniqueInts);
     }
 
     private static void copyNonEmptyLines() {
         System.out.println("\n6. Копирование не пустых строк");
 
         String[] strings = {"    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", ""};
-        String[] nonEmptyStrings = new String[countNonEmptyStrings(strings)];
-        len = strings.length;
-        int index2 = 0;
-        for (int i = 0; i < len; i++) {
-            int j = i;
-            if (!strings[i].trim().isBlank()) {
-                int countNonEmptyStrings = 1;
-                while (!strings[j + 1].trim().isBlank()) {
-                    countNonEmptyStrings ++;
-                    j++;
-                }
-                System.arraycopy(strings, i, nonEmptyStrings, index2, countNonEmptyStrings);
-                index2 += countNonEmptyStrings;
+        // вычисляем количество непустых строк и создаем второй массив
+        int count = 0;
+        for (String s : strings) {
+            if (!s.trim().isBlank()) {
+                count++;
             }
-            i = j;
         }
+        String[] nonEmptyStrings = new String[count];
 
+        // копируем непустые последовательности из первого массива во второй
+        int index = 0;
+        int start = 0;
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].trim().isBlank()) {
+                if (start != i) {
+                    System.arraycopy(strings, start, nonEmptyStrings, index, i - start);
+                    index += i - start;
+                }
+                start = i + 1;
+            }
+        }
+        if (start != strings.length) {
+            System.arraycopy(strings, start, nonEmptyStrings, index, strings.length - start);
+        }
+        // выводим оба массива на экран
         System.out.println("Исходный массив: " + Arrays.toString(strings));
         System.out.println("Массив без пустых строк: " + Arrays.toString(nonEmptyStrings));
     }
 
-    private static void printFloatArray(float @NotNull [] floats) {
+    private static void printFloatArray(float[] floats) {
         len = floats.length;
         for (int i = 0; i < len; i++) {
             System.out.printf("%7.3f", floats[i]);
@@ -150,7 +159,7 @@ public class ArrayTheme {
         System.out.println();
     }
 
-    private static boolean isUnique(int @NotNull [] ints, int num) {
+    private static boolean isUnique(int[] ints, int num) {
         for (int currentNum : ints) {
             if (currentNum == num) {
                 return true;
@@ -159,20 +168,10 @@ public class ArrayTheme {
         return false;
     }
 
-    private static void printIntArray(int @NotNull [] ints) {
+    private static void printIntArray(int[] ints) {
         for (int num: ints) {
             System.out.print(num + " ");
         }
         System.out.println();
-    }
-
-    private static int countNonEmptyStrings(String @NotNull [] strings) {
-        int count = 0;
-        for (String s : strings) {
-            if (!s.trim().isBlank()) {
-                count++;
-            }
-        }
-        return count;
     }
 }
