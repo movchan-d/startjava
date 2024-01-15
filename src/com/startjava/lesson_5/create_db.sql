382973d8ed853a36fe30a49541823eae32815c41
movchan-d <den.a.movchan@gmail.com>
2024-01-15 16:51:27 +0300

    Lesson_5. HW.

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,18 @@
+\c robots;
+
+DROP TABLE IF EXISTS jaegers;
+
+CREATE TABLE jaegers (
+  id         SERIAL PRIMARY KEY,
+  model_name TEXT,
+  mark       INT,
+  height     NUMERIC,
+  weight     NUMERIC,
+  status     TEXT,
+  origin     TEXT,
+  launch     DATE,
+  kaiju_kill INT
+);
+
+\ir 'init_db.sql'
+\ir 'queries.sql'
---
+++ src/com/startjava/lesson_5/queries.sql
@@ -0,0 +1,51 @@
+\echo Все роботы
+SELECT *
+  FROM jaegers;
+
+\echo Не уничтоженные роботы
+SELECT *
+  FROM jaegers
+ WHERE status = 'Active';
+
+\echo Роботы серий Mark-1 и Mark-4
+SELECT *
+  FROM jaegers
+ WHERE mark IN (1, 4);
+
+\echo Роботы кроме серий Mark-1 и Mark-4 (сортировка Mark по убыванию)
+SELECT *
+  FROM jaegers
+ WHERE mark NOT IN (1, 4)
+ ORDER BY mark DESC;
+
+\echo Самый старый робот
+SELECT *
+  FROM jaegers
+ WHERE launch = (SELECT MIN(launch)
+				   FROM jaegers);
+ 
+\echo Роботы, которые уничтожили больше всех kaiju (сортировка kaiju по убыванию)
+SELECT *
+  FROM jaegers
+ WHERE kaiju_kill = (SELECT MAX(kaiju_kill)
+					   FROM jaegers)
+ ORDER BY kaiju_kill DESC;
+
+\echo Средний вес роботов, округленный до 3-х знаков
+SELECT ROUND(AVG(weight), 3) AS avg_weight
+  FROM jaegers;
+
+\echo Увеличение на 1 kaiju у неуничтоженных роботов
+UPDATE jaegers
+   SET kaiju_kill = kaiju_kill + 1
+ WHERE status = 'Active';
+
+SELECT *
+  FROM jaegers;
+  
+\echo Удаление уничтоженных роботов
+DELETE FROM jaegers
+ WHERE status = 'Destroyed';
+ 
+SELECT *
+  FROM jaegers;
